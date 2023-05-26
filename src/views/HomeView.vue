de41b0c244c726857e7ca546c144b56011afc486
abukbt13 <abukbt13@gmail.com>
2023-05-26 09:30:56 +0300

    changed home page

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,9 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+ <h2>My todays planning 26/05/2023</h2>
+ <p>Good morning Benard</p>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 
-export default {
-  name: 'HomeView',
-  components: {
-    HelloWorld
-  }
-}
 </script>
