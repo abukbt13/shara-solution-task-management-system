b6073609e05a5ac4c30f18aaf2d1f14a8458863b
abukbt13 <abukbt13@gmail.com>
2023-05-26 09:57:39 +0300

    started the homepage

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,27 @@
 <template>
- <h2>My todays planning 26/05/2023</h2>
- <p>Good morning Benard</p>
+  <div class="container">
+    
+        <h2>My todays planning 26/05/2023</h2>
+        <p>Good morning Benard</p>
+        <div class="main d-flex ">
+          <div class="tasks w-50">
+              <h2>Todays Tasks</h2>
+          </div>
+          <div class="reviews ">
+              <h2 class="text-align-left">Reviews</h2>
+          </div>
+        </div>
+</div>
+
 </template>
 
 <script>
 // @ is an alias to /src
 
 </script>
+<style>
+.main{
+  width: 100vw;
+  height: 73vh;
+}
+</style>
