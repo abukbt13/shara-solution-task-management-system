70fb7b97cda6f9baa467e310ae8d8f5a645d1fba
Benalagat <benardkiplagat2000@mail.com>
2023-07-06 11:48:35 +0300

    projectuser

--- src/views/Admin/manage/Tasks.vue
+++ src/views/Admin/manage/Tasks.vue
@@ -22,7 +22,7 @@ const getTodo = async () =>{
   }
 }
 const getUsers= async () =>{
-  const res = await axios.get('http://127.0.0.1:8000/api/users_to_be_added_to_task',{headers})
+  const res = await axios.get('http://127.0.0.1:8000/api/usersNotAssignedTask/'+project_id,{headers})
   if(res.status===200){
     users.value=  res.data
   }
@@ -109,49 +109,45 @@ onMounted(()=>{
           <button type="button" class="btn-close btn-lg btn-danger" data-bs-dismiss="modal" aria-label="Close"></button>
         </div>
         <div class="modal-body">
+          
           <form @submit.prevent="createTask">
-            <div class="form d-flex">
+    <div class="mb-3">
+        <label>Name</label>
+        <input type="name" class="form-control" v-model="task">
+    </div>  
+    <div class="mb-3">
+        <label for="">Add User</label><br>
+        <select  v-model="user_id" class="form-select">
+            <option v-for="user in users" :value="user.id" :key="user.id">{{ user.name }}</option>
+        </select>
+    </div>
+        <label class="mb-3" for="">Deadline</label><br>
+        <div class="mb-3">
+          <div class="row">
+          <div class="col">
+            <div class="col-5">
               <div class="group">
-                <label for="">Task Name</label><br>
-                <textarea  style="outline-color: #FDB0A0; width: 13rem; color: #2C3A47; " rows="1" cols="4" v-model="task"></textarea>
-                <p class="text-danger text-lg-start">*required </p>
-              </div>
-              <div class="group ms-2">
-                <label for="">Add User</label><br>
-                <select v-model="user_id" style="outline-color: #FDB0A0; color: #2C3A47; width: 13rem; height: 2rem">
-                  <option v-for="user in users" :value="user.id" :key="user.id">{{ user.name }}</option>
-                </select>
-
-              </div>
+              <label for="">Date</label><br>
+              <input v-model="date_line" type="date" class="form-control">
+          </div>
             </div>
-            <div class="form p-2 d-flex">
+            <div class="col-5">
               <div class="group">
-                <label for="">Deadline</label><br>
-              </div>
+            <label for="">Time</label><br>
+            <input v-model="time_line"  type="time" class="form-control">
+         </div>
             </div>
-            <div class="form p-2 d-flex">
-              <div class="group">
-                <label for="">Date</label><br>
-                <input v-model="date_line" style="outline-color: #FDB0A0; color: #2C3A47; height: 2rem; width: 13rem;" type="date" name="" id="">
-              </div>
-              <div class="group">
-                <label for="">Time</label><br>
-                <input v-model="time_line" style="margin-left: 1rem; color: #2C3A47; height: 2rem; outline-color: #FDB0A0; width: 13rem;" type="time" name="" id="">
-              </div>
-            </div>
-            <div class="form p-2 d-flex">
-              <div class="group">
-                <label for="">Description</label><br>
-                <textarea v-model="description" style="border-radius:0px; color: #6C1705;color: #2C3A47; outline-color: #FDB0A0;" id="" cols="41" rows="5"></textarea>
-                <p class="text-danger text-lg-start">*required </p>
-              </div>
-            </div>
-            <div class="form p-2 d-flex">
-              <div class="group">
-                <button style="border-radius: 0px; font-size: 22px;" type="submit" class="btn btn-success">Create</button>
-              </div>
-            </div>
-          </form>
+          </div>
+        </div>
+        </div>
+     <div class="group mb-3">
+        <label for="">Description</label><br>
+        <textarea v-model="description"  rows="5" class="form-control"></textarea>
+     </div>
+     <div class="group mb-3">
+        <button style="border-radius: 0px; font-size: 22px;" type="submit" class="btn btn-success">Create</button>
+    </div>
+    </form>
         </div>
       </div>
     </div>
---
+++ src/views/Admin/manage/assign.vue
@@ -0,0 +1,69 @@
+<template>
+    <form @submit.prevent="createTask">
+    <div class="mb-3">
+        <label>Name</label>
+        <input type="name" class="form-control" v-model="task">
+    </div>  
+    <div>
+        <label for="">Add User</label><br>
+        <select class="form-select">
+            <option v-for="user in users" :value="user.id" :key="user.id">{{ user.name }}</option>
+        </select>
+    </div>
+    <div class="group">
+        <label for="">Deadline</label><br>
+        <div class="group">
+            <label for="">Date</label><br>
+            <input v-model="date_line" type="date" class="form-control">
+        </div>
+       <div class="group">
+            <label for="">Time</label><br>
+            <input v-model="time_line"  type="time" class="form-control">
+         </div>
+     </div>
+     <div class="group">
+        <label for="">Description</label><br>
+        <textarea v-model="description"  rows="5" class="form-control"></textarea>
+     </div>
+     <div class="group">
+        <button style="border-radius: 0px; font-size: 22px;" type="submit" class="btn btn-success">Create</button>
+    </div>
+    </form>
+    <form @submit.prevent="createTask">
+            <div class="form d-flex">
+              <div class="group ms-2">
+                <label for="">Add User</label><br>
+                <select v-model="user_id" style="outline-color: #FDB0A0; color: #2C3A47; width: 13rem; height: 2rem">
+                  <option v-for="user in users" :value="user.id" :key="user.id">{{ user.name }}</option>
+                </select>
+              </div>
+            </div>
+            <div class="form p-2 d-flex">
+              <div class="group">
+                <label for="">Deadline</label><br>
+              </div>
+            </div>
+            <div class="form p-2 d-flex">
+              <div class="group">
+                <label for="">Date</label><br>
+                <input v-model="date_line" style="outline-color: #FDB0A0; color: #2C3A47; height: 2rem; width: 13rem;" type="date" name="" id="">
+              </div>
+              <div class="group">
+                <label for="">Time</label><br>
+                <input v-model="time_line" style="margin-left: 1rem; color: #2C3A47; height: 2rem; outline-color: #FDB0A0; width: 13rem;" type="time" name="" id="">
+              </div>
+            </div>
+            <div class="form p-2 d-flex">
+              <div class="group">
+                <label for="">Description</label><br>
+                <textarea v-model="description" style="border-radius:0px; color: #6C1705;color: #2C3A47; outline-color: #FDB0A0;" id="" cols="41" rows="5"></textarea>
+                <p class="text-danger text-lg-start">*required </p>
+              </div>
+            </div>
+            <div class="form p-2 d-flex">
+              <div class="group">
+                <button style="border-radius: 0px; font-size: 22px;" type="submit" class="btn btn-success">Create</button>
+              </div>
+            </div>
+          </form>
+</template>
